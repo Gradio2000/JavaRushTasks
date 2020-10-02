5b45305f5322fe5db7acc2fca87f8ed3687b336d
Gradio2000 <laskin999@gmail.com>
2020-10-02 19:43:51 +0500

    proectTest

--- 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0929/Solution.java
@@ -9,11 +9,20 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        InputStream fileInputStream = null;
+        for (int i = 0; i < 2; i++) {
+            String sourceFileName = reader.readLine();
+            try {
+                fileInputStream = getInputStream(sourceFileName);
+                break;
+            }
+            catch (FileNotFoundException e){
+                System.out.println("Файл не существует.");
+            }
+        }
 
-        String sourceFileName = reader.readLine();
+
         String destinationFileName = reader.readLine();
-
-        InputStream fileInputStream = getInputStream(sourceFileName);
         OutputStream fileOutputStream = getOutputStream(destinationFileName);
 
         while (fileInputStream.available() > 0) {
