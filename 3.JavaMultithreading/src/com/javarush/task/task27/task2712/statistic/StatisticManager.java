dc2152190ba6f70db33e4cf9ab550765f33e296e
Gradio2000 <laskin999@gmail.com>
2020-11-24 19:20:50 +0500

    proectTest

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task27.task2712.statistic;
+
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StatisticManager {
+    private static StatisticManager statisticManager;
+    private StatisticStorage statisticStorage = new StatisticStorage();
+    private StatisticManager() {
+    }
+    public static StatisticManager getInstance(){
+        if (statisticManager == null){
+            statisticManager = new StatisticManager();
+        }
+        return statisticManager;
+    }
+
+    public void register(EventDataRow data){
+
+    }
+
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            for (EventType type : EventType.values()){
+                storage.put(type, new ArrayList<>());
+            }
+        }
+    }
+
+}
