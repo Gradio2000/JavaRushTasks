1292a6013153bac2d6810dc6d1dcd44e0b86b682
Gradio2000 <laskin999@gmail.com>
2021-01-10 12:07:34 +0500

    16 task (2)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,11 +1,17 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
+
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(1);
+        Cook cook = new Cook("Alex");
+        tablet.addObserver(cook);
         tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
+
+
+//        tablet.createOrder();
+//        tablet.createOrder();
+//        tablet.createOrder();
     }
 }
