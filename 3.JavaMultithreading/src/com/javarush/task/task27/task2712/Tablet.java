20713156a3893002b112d8c31a9222ed4ba0a7c4
Gradio2000 <laskin999@gmail.com>
2021-01-10 12:09:54 +0500

    Revert "16 task (2)"
    This reverts commit 1292a601

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,11 +3,10 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
-import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet extends Observable {
+public class Tablet {
     private final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
@@ -15,17 +14,13 @@ public class Tablet extends Observable {
         this.number = number;
     }
 
-    public Order createOrder(){
-        Order order = null;
+    public void createOrder(){
         try {
-            order = new Order(this);
+            Order order = new Order(this);
             ConsoleHelper.writeMessage(order.toString());
-            this.setChanged();
-            notifyObservers(order);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
-        return order;
     }
 
     @Override
